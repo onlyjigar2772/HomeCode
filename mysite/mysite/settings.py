c5b1ea09db6494e8d66f724701e2734150f72b88
Jigar Shah <onlyjigar2772@gmail.com>
2015-04-05 22:17:35 +0530

    Finished part - 1 of tutorial

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uhhu_r&+q@u-bss*=te-@em%zdps(or_(ulbxsfdu_+57fv00a'
+SECRET_KEY = 'p-k8fuyn%_48ri(-t3sr8m-s6lzjj)&)627m=id8$$2gkeolja'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+    'polls'
 )
 
 MIDDLEWARE_CLASSES = (
